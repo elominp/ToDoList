dd1661fa9c70c11c3d5e92fa6607fe26da84f954
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2017-01-22 13:10:36 +0100

    git init

---
+++ app/src/main/java/com/epitech/elominp/epitechstodolist/MainActivity.java
@@ -0,0 +1,99 @@
+package com.epitech.elominp.epitechstodolist;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView todoListView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Toolbar configuration */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        /* ToDoListView configuration */
+        todoListView = (ListView) findViewById(R.id.todolistView);
+
+        /* Floating add button configuration */
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Poot is Ded!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    // Called when displayed, after creating the activity or after resuming app
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    // Called when activity start interacting with the user
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    // Called when another activity comes into the foreground
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    // Called when the activity is no longer visible to the user
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    // Called when the activity is finished or being destroyed
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
