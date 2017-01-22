18746037c106f2f3dc118ecea7d69d5f4a742eee
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2017-01-22 13:51:54 +0100

    clean MainActivity and write javadoc

--- app/src/main/java/com/epitech/elominp/epitechstodolist/MainActivity.java
+++ app/src/main/java/com/epitech/elominp/epitechstodolist/MainActivity.java
@@ -13,24 +13,37 @@ import android.widget.ExpandableListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Called when the Activity is created, so at the starting of the application.
+     * It configures the display and show the ToDoList.
+     *
+     * @param savedInstanceState the saved instance state of the activity
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Toolbar configuration */
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        setupMenuBar();
+        setupListeners();
+    }
 
+    /**
+     * Sets up the listeners used in this activity :
+     * - The listener for clicking an item
+     * - The listener for holding an item
+     * - The listener for clicking on the add button
+     */
+    protected void setupListeners() {
         /* ToDoListView configuration */
         ExpandableListView todoListView = (ExpandableListView) findViewById(R.id.todolistView);
+        /* A simple click on an item expand it */
         todoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
             }
         });
+        /* A long click on an item display a menu to edit it / remove it / change it's status */
         todoListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -40,46 +53,83 @@ public class MainActivity extends AppCompatActivity {
 
         /* Floating add button configuration */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /* Clicking on the floating add button will go the activity creating a new item */
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Poot is Ded!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+    }
+
+    /**
+     * Sets up the menu bar.
+     */
+    protected void setupMenuBar() {
+        /* Toolbar configuration */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+
+    /**
+     * Clear the ListView, fetch the list from the database and put it into the ListView.
+     */
+    protected void refreshToDoList() {
 
     }
 
-    // Called when displayed, after creating the activity or after resuming app
+    /**
+     * Called when displayed, after creating the activity or after resuming app.
+     * Currently not being used.
+     */
     @Override
     protected void onStart() {
         super.onStart();
     }
 
-    // Called when activity start interacting with the user
+    /**
+     * Called when activity start interacting with the user.
+     * Currently not being used.
+     * Could be used in the future to refresh the list if it's synchronized with other sources.
+     */
     @Override
     protected void onResume() {
         super.onResume();
     }
 
-    // Called when another activity comes into the foreground
+    /**
+     * Called when another activity comes into the foreground.
+     * Currently not being used.
+     */
     @Override
     protected void onPause() {
         super.onPause();
     }
 
-    // Called when the activity is no longer visible to the user
+    /**
+     * Called when the activity is no longer visible to the user.
+     * Currently not being used.
+     */
     @Override
     protected void onStop() {
         super.onStop();
     }
 
-    // Called when the activity is finished or being destroyed
+    /**
+     * Called when the activity is being destroyed.
+     * Currently not being used.
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
 
+    /**
+     * Called when the activity is being restarted.
+     * Currently not being used.
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
