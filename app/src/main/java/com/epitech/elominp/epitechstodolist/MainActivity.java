b860669bbc8a46ee4c96bd2081e151eadaeeaec8
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2017-01-22 19:43:20 +0100

    tested database, it works

--- app/src/main/java/com/epitech/elominp/epitechstodolist/MainActivity.java
+++ app/src/main/java/com/epitech/elominp/epitechstodolist/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ExpandableListView;
 
 public class MainActivity extends AppCompatActivity {
+    private TodoListStorage storage = TodoListStorage.getInstance();
 
     /**
      * Called when the Activity is created, so at the starting of the application.
@@ -23,9 +25,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupStorage();
         setupMenuBar();
         setupTodoList();
         setupListeners();
+    }
+
+    protected void setupStorage() {
+        storage.setContext(this.getApplicationContext());
     }
 
     /**
